a8effdc179c9d7535e316d35570108091ad4e440
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-10 17:18:00 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,19 @@
+(function() {
+
+    $('#NewBox').load('main.html')
+
+    $('.topmenu > a').on('click', function() {
+        e.preventDefault()
+        var url = $(this).attr('href')
+        $('#NewContainer').remove()
+        $('#NewBox').load(url)
+    })
+
+
+    $('.h1Nav > h1 > a').on('click', function(){
+        e.preventDefault()
+        $('#NewContainer').remove()
+        $('#NewBox').load(url)
+    })
+
+})(jQuery)
