4af60d3bd9832ba8fb55bbecc535f9b37f03e9ea
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-11 18:04:41 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,22 @@
 (function() {
 
+    // mousewheel 이벤트 연결
+    $('section').on('mousewheel', function(e, wh) {
+        // 마우스 휠 올릴때
+        if ( wh > 0 ) {
+            var prev = $(this).prev().offset().top
+            $('html, body').stop().animate({
+                scrollTop: prev
+            }, 800)
+        } else if ( wh < 0 ) {
+            var next = $(this).next().offset().top
+            $('html, body').stop().animate({
+                scrollTop: next
+            }, 800)
+        }
+    })
+
+    // #newBox에 각 html 삽입
     $('#newBox').load('main.html')
 
     $('.topmenu a').on('click', function(e) {
