48668dfed3ed8d1bb70ef81ef5c63a8ce6ebda56
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-15 11:17:01 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,13 +1,28 @@
-(function() {
+(function($) {
+
+    // loading 화면
+    var minCnt = setInterval(minusCount, 1000)
+    var k = 5;
+    function minusCount() {
+        k--;
+        if (k===0) {
+            clearInterval(minCnt)
+            $('.introAni').fadeOut(500)
+            return false
+        }
+    }
+
 
     // mousewheel 이벤트 연결
-    $('section').on('mousewheel', function(e, wh) {
+    $("section").on("mousewheel", function (e, wh) {
+        var index = $(this).index()
         // 마우스 휠 올릴때
         if ( wh > 0 ) {
             var prev = $(this).prev().offset().top
             $('html, body').stop().animate({
                 scrollTop: prev
             }, 800)
+        // 마우스 휠 내릴때
         } else if ( wh < 0 ) {
             var next = $(this).next().offset().top
             $('html, body').stop().animate({
@@ -34,7 +49,7 @@
         $('#newContainer').remove()
         $('#newBox').load(url)
     })
-    
+
     // depth2 클릭시 서브페이지 연결
     $('.depth2 > li > a').on('click', function(e) {
         e.preventDefault()
