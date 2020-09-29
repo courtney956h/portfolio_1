fc276ac9de65aa5a62934f733e2aa8967fc64dfc
courtney956h <68522358+courtney956h@users.noreply.github.com>
2020-09-29 15:10:29 +0900

    반응형 추가

--- js/common.js
+++ js/common.js
@@ -1,5 +1,40 @@
 (function($) {
 
+    /* 아코디언 이벤트 */
+    $('.depth1 > li').on('click', function(){
+        $(this).toggleClass('on')
+        $(this).find('.depth2').slideToggle(300)
+        $(this).siblings().each(function(){ 
+            if ( $(this).find('.depth2').css('display') === 'block' ) {
+              $(this).find('.depth2').slideUp(300)
+              $(this).removeClass('on')
+            }
+        })
+    })
+
+
+    // 메뉴 버튼
+    $('.h1Nav .open_nav').on('click', function(){
+        $(this).next().stop().slideDown(300)
+        $(this).hide()
+        $(this).nextAll('.close_nav').css({
+            display:'block'
+        })
+    })
+
+    $('.h1Nav .close_nav').on('click', function() {
+        $(this).prev().stop().slideUp(300)
+        $(this).hide()
+        $(this).prevAll('.open_nav').css({
+          display: 'block'
+        })
+        $('.depth2').hide()
+        $('.h1Nav .nav .depth1 > li').removeClass('on')
+      })
+
+
+
+
 
     // loading 화면
     $(".introAni").addClass("on");
@@ -209,20 +244,19 @@
 
 
     // mousewheel 이벤트 연결
-    $('section').on("mousewheel", function (e, wh) {
-        var index = $(this).index()
+    $('.section').on("mousewheel", function (e, wh) {
         // 마우스 휠 올릴때
         if ( wh > 0 ) {
-            var prev = $(this).prev().offset().top
+            var prev = $(this).prev().offset().top;
             $('html, body').stop().animate({
                 scrollTop: prev
-            }, 800)
+            }, 800, 'linear')
         // 마우스 휠 내릴때
         } else if ( wh < 0 ) {
-            var next = $(this).next().offset().top
+            var next = $(this).next().offset().top;
             $('html, body').stop().animate({
                 scrollTop: next
-            }, 800)
+            }, 800, 'linear')
         }
     })
 
@@ -238,20 +272,20 @@
     })
 
     // depth1 클릭시 서브페이지 연결
-    $('.depth1 > li > a').on('click', function(e) {
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#newContainer').remove()
-        $('#newBox').load(url)
-    })
+    // $('.depth1 > li > a').on('click', function(e) {
+    //     e.preventDefault()
+    //     var url = $(this).attr('href')
+    //     $('#newContainer').remove()
+    //     $('#newBox').load(url)
+    // })
 
     // depth2 클릭시 서브페이지 연결
-    $('.depth2 > li > a').on('click', function(e) {
-        e.preventDefault()
-        var url = $(this).attr('href')
-        $('#newContainer').remove()
-        $('#newBox').load(url)
-    })
+    // $('.depth2 > li > a').on('click', function(e) {
+    //     e.preventDefault()
+    //     var url = $(this).attr('href')
+    //     $('#newContainer').remove()
+    //     $('#newBox').load(url)
+    // })
 
     // $('.h1Nav > h1 > a').on('click', function(){
     //     $('#newContainer').remove()
